6f328e32f7478cde6b4f7e2fc55dad6ca0414dfa
gavin <gavinz0228@gmail.com>
2018-08-29 22:10:45 -0400

    completed projects section

--- home_page/src/components/Projects.js
+++ home_page/src/components/Projects.js
@@ -3,18 +3,28 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Grid from '@material-ui/core/Grid';
 const styles = theme => ({
   title:{
       paddingLeft: 50,
       paddingTop: 20
   },
-  content:{
-      marginLeft:240
-  },
   divider:{
       margin:10
+  },
+  links:{
+      paddingTop:10,
+      paddingBottom:10,
+      paddingLeft:2,
+      paddingRight:2
+  },
+  divider:{
+    margin: 10
   }
 });
 class Projects extends Component {
@@ -22,13 +32,29 @@ class Projects extends Component {
       return (
         <Card >
             <CardContent>
-            <Typography gutterBottom variant="headline" component="h2" className={this.props.classes.title}>
-                Projects
-            </Typography>
-            <Typography component="p" className={this.props.classes.content} >
-                I'm a full stack developer in New York City. 
-                I like all kinds of computer stuff and various types of sports.
-            </Typography>
+              <Typography gutterBottom variant="headline" component="h2" className={this.props.classes.title}>
+                      Projects
+              </Typography>
+              <Grid container>
+                <Grid xs={3} align="center">
+                </Grid>
+                <Grid xs={8}>
+                  <List component="nav">
+                    <ListItem  className={this.props.classes.links} button component="a" href="https://github.com/gavinz0228/SharpContentScraper">
+                      <ListItemText primary="Sharp Content Scraper - A Linq style web scraper library written for C#" />
+                    </ListItem>
+                    <Divider/>
+                    <ListItem  className={this.props.classes.links} button component="a" href="https://github.com/gavinz0228/MLPractice">
+                      <ListItemText primary="Machine Learning Practices - A few machine learning practice projects with python and Keras " />
+                    </ListItem>
+                    <Divider/>
+                    <ListItem  className={this.props.classes.links} button component="a" href="https://github.com/gavinz0228/WebQuest-Interpreter">
+                      <ListItemText primary="WebQuest Interpreter  - A interpreter written in C++ for debugging and testing HTTP Request restful API for an language called WebQuest created by myself " />
+                    </ListItem>
+                    <Divider/>
+                  </List>
+                </Grid>
+              </Grid>
             </CardContent>
             
         </Card>
